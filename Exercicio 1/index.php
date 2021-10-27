d3c3a194221d6193bb873b5b2d52b35e301a15b5
Arthur <arthur.coelhojb@gmail.com>
2021-10-26 23:45:48 -0300

    Acredito que agora esta correto!

--- Exercicio 1/index.php
+++ Exercicio 1/index.php
@@ -1,24 +1,4 @@
-<?php
 
-	$value1 = $value2 = '';	
-
-	if (key_exists('value1', $_POST) && key_exists('value2', $_POST)) {
-		$value1 = $_POST['value1'];
-		$value2 = $_POST['value2'];
-		
-		$sum = $value1 + $value2;
-		
-		if ($sum > 20){
-			$sum = $sum + 8;
-		}
-		
-		if ($sum <= 20){
-			$sum = $sum - 5;
-		}
-		
-		
-	}
-?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -61,7 +41,8 @@
 		<?php
 			$value1 = $value2 = '';	
 
-            if (key_exists('value1', $_POST) && key_exists('value2', $_POST)) {
+            if(isset($_POST['value1']) &&($_POST['value1'])) {
+
                 $value1 = $_POST['value1'];
                 $value2 = $_POST['value2'];
                 
