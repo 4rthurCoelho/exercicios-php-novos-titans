22fc7b8729965beaede505cacbb2cf1d56be8df3
Arthur <arthur.coelhojb@gmail.com>
2021-10-29 00:37:14 -0300

    Html e Css feitos mas não esta printando os resultados!

--- Exercicio 3/index.php
+++ Exercicio 3/index.php
@@ -1,13 +1,85 @@
-<?php
-	$nome = $argv[1];
-    $sexo =  $argv[2];
-	$idade = $argv[3];
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Exercicio-03</title> 
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+	<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+<body>
+	
+<main class= "container">
+   
+	<h2>Exercício 1</h2>
 
-	if ($sexo == "F" && $idade <= 25) {
-		echo "Parabens $nome você foi ACEITA:)";
-	}
-	else {
-		echo "$nome você não foi Aceita(o) pois não atende os requisitos do projeto:(";
-	}
+    <p>Coloque os 2 valores, o algoritimo vai ler os 2 números e efetuar a adição. Caso o valor somado seja maior que 20, 
+	vai ser apresentando a soma dele mais 8; caso o valor somado seja menor ou igual a 20, sera apresentado a soma subtraindo-se 5.</p>
 
-?>
+    <br />
+
+		
+		<?php
+
+			$nome = "";
+			$sexo = "";
+			$idade = "";
+
+			if(isset($_POST['valeu1']) && ($_POST['value2']) && ($_POST['value3'])) {
+				
+				$nome = $_POST['value1'];
+				$sexo = $_POST['value2'];
+				$idade = $_POST['value3'];
+			
+				if ($sexo == "F" && $idade <= 25) {
+					echo "<div class='results'>Parabens $nome você foi ACEITA:)</div>";
+				}
+				
+				else if ($sexo == "f" && $idade <= 25) {
+					echo "<div class='results'>Parabens $nome você foi ACEITA:)</div>";
+				}
+
+				else if ($sexo == "Feminino" && $idade <= 25) {
+					echo "<div class='results'>Parabens $nome você foi ACEITA:)</div>";
+				}
+
+				else if ($sexo == "feminino" && $idade <= 25) {
+					echo "<div class='results'>Parabens $nome você foi ACEITA:)</div>";
+				}
+
+				else {
+					echo "<div class='results'>$nome você não foi Aceita(o) pois não atende os requisitos do projeto:(</div>";
+				}
+			
+			}
+
+          
+		?>
+
+	<form id="formulario" action="index.php" method="POST">
+		<div class="input-field">
+			<label for="text">Nome</label><br />
+			<input type="text" name="value1" value="<?= $nome ?>" id="nome" autocomplete="off" placeholder="Escreva seu nome aqui! "/>
+			<div class="underline"></div>
+  		</div>
+		<div class="input-field">
+			<label for="text">Sexo</label><br />
+			<input type="text" name="value2" value="<?= $sexo ?>" id="sexo" autocomplete="off" placeholder="Qual é o seu sexo? (F/M)"/>
+			<div class="underline"></div>
+		</div>	
+	    <div class="input-field"><br />
+			<label for="number">Idade</label><br />
+			<input type="number" name="value3" value="<?= $idade ?>" id="idade" autocomplete="off" placeholder="Qual é a sua idade?"/>
+			<div class="underline"></div>
+			
+
+		</div>	
+
+		<input type="submit" name="enviar" value="Enviar"/>	
+		
+    </form>
+
+</main>
+</body>
+</html>
