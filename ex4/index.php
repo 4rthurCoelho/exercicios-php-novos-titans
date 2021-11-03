34eab642bf231716cc31d025b9b1f9a17da6a858
Arthur <arthur.coelhojb@gmail.com>
2021-11-03 19:11:15 -0300

    Adicionado html e css ao php!

--- ex4/index.php
+++ ex4/index.php
@@ -1,24 +1,80 @@
-<?php 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Exercicio-04</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+	<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+<body>
 
-		$num1 = $argv[1];
-		$num2 = $argv[2];
-		$num3 = $argv[3];
+<main class= "container">
+   
+	<h2>Exercício 4</h2>
 
-		if	   ($num1 > $num2 && $num2 > $num3) echo "$num1 $num2 $num3";
-		elseif ($num3 > $num2 && $num2 > $num1) echo "$num3 $num2 $num1";
-		elseif ($num2 > $num1 && $num1 > $num3) echo "$num2 $num1 $num3";
-		elseif ($num2 > $num1 && $num1 < $num3) echo "$num2 $num3 $num1";
-		elseif ($num1 > $num3 && $num3 > $num2) echo "$num1 $num3 $num2";
-		elseif ($num3 > $num1 && $num1 > $num2) echo "$num3 $num1 $num2";
-		elseif ($num3 > $num1 && $num1 > $num2) echo "$num3 $num1 $num2";
-		elseif ($num1 = $num2 && $num2 > $num3) echo "Dois valores iguais não são aceitos :(";
-		elseif ($num3 = $num2 && $num2 > $num1) echo "Dois valores iguais não são aceitos :(";
-		elseif ($num2 = $num1 && $num1 > $num3) echo "Dois valores iguais não são aceitos :(";
-		elseif ($num2 = $num1 && $num1 < $num3) echo "Dois valores iguais não são aceitos :(";
-		elseif ($num1 = $num3 && $num3 > $num2) echo "Dois valores iguais não são aceitos :(";
-		elseif ($num3 = $num1 && $num1 > $num2) echo "Dois valores iguais não são aceitos :(";
-		elseif ($num3 = $num1 && $num1 > $num2) echo "Dois valores iguais não são aceitos :(";
+    <p><br />Entrar com 3 números e o algoritimo ira imprimi-los em ordem decrescente (suponha números diferentes) :)<br /></p>
 
+    <br />
 
+	<form id="formulario" action="index.php" method="post">
+		<div class="input-field">
+			<label for="number">Número 1</label><br />
+			<input type="number" name="value1" value="<?= $num1 ?>" id="valor1" autocomplete="off" placeholder="Adicione um numero!"/>
+			<div class="underline"></div>
+  		</div>
+		
+		<div class="input-field">
+			<label for="number">Número 2</label><br />
+			<input type="number" name="value2" value="<?= $num2 ?>" id="valor2" autocomplete="off" placeholder="Adicione um numero!"/>
+			<div class="underline"></div>
+  		</div>
+		
+		<div class="input-field"><br />
+			<label for="number">Número 3</label><br />
+			<input type="number" name="value3" value="<?= $num3 ?>" id="valor3" autocomplete="off" placeholder="Adicione um numero!"/>
+			<div class="underline"></div>
+  		</div>
+		
+		  <input type="submit" name="enviar" value="Enviar" class="enviar"/>	
 
- ?>
+	
+		
+    </form>
+
+	<?php
+
+		$num1 = '';
+		$num2 = '';
+		$num3 = '';	
+
+		if(isset($_POST['value1']) && ($_POST['value2']) && ($_POST['value3'])) {
+
+		$num1 = $_POST['value1'];
+		$num2 = $_POST['value2'];
+		$num3 = $_POST['value3'];
+
+		if	   ($num1 > $num2 && $num2 > $num3) echo "<div class='results'>$num1 $num2 $num3</div>";
+		elseif ($num3 > $num2 && $num2 > $num1) echo "<div class='results'>$num3 $num2 $num1</div>";
+		elseif ($num2 > $num1 && $num1 > $num3) echo "<div class='results'>$num2 $num1 $num3</div>";
+		elseif ($num2 > $num1 && $num1 < $num3) echo "<div class='results'>$num2 $num3 $num1</div>";
+		elseif ($num1 > $num3 && $num3 > $num2) echo "<div class='results'>$num1 $num3 $num2</div>";
+		elseif ($num3 > $num1 && $num1 > $num2) echo "<div class='results'>$num3 $num1 $num2</div>";
+		elseif ($num3 > $num1 && $num1 > $num2) echo "<div class='results'>$num3 $num1 $num2</div>";
+		elseif ($num1 = $num2 && $num2 > $num3) echo "<div class='results'>Dois valores iguais não são aceitos :(</div>";
+		elseif ($num3 = $num2 && $num2 > $num1) echo "<div class='results'>Dois valores iguais não são aceitos :(</div>";
+		elseif ($num2 = $num1 && $num1 > $num3) echo "<div class='results'>Dois valores iguais não são aceitos :(</div>";
+		elseif ($num2 = $num1 && $num1 < $num3) echo "<div class='results'>Dois valores iguais não são aceitos :(</div>";
+		elseif ($num1 = $num3 && $num3 > $num2) echo "<div class='results'>Dois valores iguais não são aceitos :(</div>";
+		elseif ($num3 = $num1 && $num1 > $num2) echo "<div class='results'>Dois valores iguais não são aceitos :(</div>";
+		elseif ($num3 = $num1 && $num1 > $num2) echo "<div class='results'>Dois valores iguais não são aceitos :(</div>";
+
+	}
+
+?>
+
+</main>
+
+</body>
+</html>
