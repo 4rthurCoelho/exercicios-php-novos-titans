2340cdb907439fe8ec1e459ad2cbd0407e3d0829
Arthur <arthur.coelhojb@gmail.com>
2021-12-09 00:09:21 -0300

    Ex8 finalizado

--- ex8/index.php
+++ ex8/index.php
@@ -1,8 +1,50 @@
-<?php
-    $numero= $argv[1];
 
-        for ($a=1; $a <= $numero; $a++) 
+                        <!DOCTYPE html>
+                        <html lang="pt-br">
+                        <head>
+                            <title>Exercicio-08</title>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" href="/css/style.css">
+                            <link rel="stylesheet" href="/ex8/ex8.css">
+                            <link
+                            rel="stylesheet"
+                            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+                        </head>
+                        <body>
+                        
+                        <main class= "container">
+                        
+                            <h2>Exercício 8</h2>
+                        
+                            <p><br /> Adicione um número que o algoritmos vai ler e mostrar todos os número de 1 até ele e o seu produto :)<br /></p>
+                        
+                            <br />
+                        
+                            <form id="formulario" action="index.php" method="post">
+                                <div class="input-field">
+                                    <label for="number">Número</label><br />
+                                    <input type="number" name="value1" value="<?= $num ?>" id="valor1" autocomplete="off" placeholder="Adicione um numero!"/>
+                                    <div class="underline"></div>
+                                </div>
+                                    <input type="submit" name="enviar" value="Enviar" class="enviar"/>	
+                            </form>
+                        
+                            <?php
+                        
+                                $num = '';
+                            
+                                if(isset($_POST['value1'])) {
+                            
+                                    $num= $_POST['value1'];
 
-            echo "$a" . " ";
+                                    for ($a=1; $a <= $num; $a++) 
 
-
+                                        echo "<div class='results'>$a<div>" . "";
+ 
+                                }
+                            ?>
+                        
+                        </main>
+                        
+                        </body>
+                        </html>
