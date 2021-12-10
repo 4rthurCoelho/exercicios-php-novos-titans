03e8962910d270482ac935e077cf5655731bf13e
Edmar Gomes <edmar.gomes@nyx.tc>
2021-12-10 15:09:50 -0300

    Criado o indice na pagina inicial do projeto.

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Indice</title>
+    </head>
+    <body>
+        <h1>Indice</h1>
+        <p><a href="/ex1">Exercicio 1</a></p>
+        <p><a href="/ex2">Exercicio 2</a></p>
+        <p><a href="/ex3">Exercicio 3</a></p>
+    </body>
+</html>
