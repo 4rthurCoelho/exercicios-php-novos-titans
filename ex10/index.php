a7cfad586001e0901a8f23ed17c8a40d5e49014e
Arthur <arthur.coelhojb@gmail.com>
2022-01-04 14:34:20 -0300

    Identação corrigida!

--- ex10/index.php
+++ ex10/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-		<html lang="pt-br">
+	<html lang="pt-br">
 		<head>
 			<title>Exercicio-10</title>
 			<meta charset="utf-8">
@@ -15,55 +15,53 @@
 			
 		<a href="../index.php" id="voltar">Voltar</a>
 		
-			<h2>Exercício 10</h2>
+		<h2>Exercício 10</h2>
 		
-			<p><br />Entre com 20 números positivos e negativos e o algoritimo vai mostrar a soma dos positivos e o total de números negativos.<br /></p>
-            <p><br />ATENÇÃO: De espaço em cada numero :)<br /></p>
+		<p><br />Entre com 20 números positivos e negativos e o algoritimo vai mostrar a soma dos positivos e o total de números negativos.<br /></p>
+        <p><br />ATENÇÃO: De espaço em cada numero :)<br /></p>
 		
-			<br />
+		<br />
 		
-			<form id="formulario" action="/ex10/index.php" method="post">
-				<div class="input-field">
-					<label for="text">Números</label><br />
-					<input type="text" name="value1" id="valor1" autocomplete="off" placeholder="Coloque os números aqui!"/>
-					<div class="underline"></div>
-				  </div>
-					 <input type="submit" name="enviar" value="Enviar" class="enviar"/>	
-			</form>
+		<form id="formulario" action="/ex10/index.php" method="post">
+			<div class="input-field">
+				<label for="text">Números</label><br />
+				<input type="text" name="value1" id="valor1" autocomplete="off" placeholder="Coloque os números aqui!"/>
+				<div class="underline"></div>
+			</div>
+			<input type="submit" name="enviar" value="Enviar" class="enviar"/>	
+		</form>
 		
-			<?php
-		
-				$vetor = '';
-                $pos = '';
-                $neg = '';		
-		
-				if(isset($_POST['value1'])) {
-                    
-                    $numeros = $_POST['value1'];
-                    
-                    $vetor= explode (" ", $numeros);
-                    $pos=array();
-                    $neg=array();
-                
-                    foreach($vetor as $vet){
-                        ($vet <= 0) ? $neg[]=$vet : $pos[]=$vet;
-                    } 
-                        echo "<div class='results'>\n</div>" . "<div class='results'>Os vetores positivos são:</div>" . "<div class='results'>\n</div>" . "<div class='results'>\n</div>";
-                            print_r($pos);
-                        
-                        echo "<div class='results'>\n</div>" . "<div class='results'>Os vetores negativos são:</div>" . "<div class='results'>\n</div>" . "<div class='results'>\n</div>";
-                            print_r($neg);
-                
-                    $total = array_sum($pos);
-                    $count = count($neg);
-                
-                        echo "<div class='results'>\n</div>" . "</div><div class='results'>Soma dos positivos: $total</div>";
-                        echo "<div class='results'>\n</div>" . "</div><div class='results'>Quantidade de negativos: $count</div>";
+		<?php
+			$vetor = '';
+			$pos = '';
+			$neg = '';		
+			
+			if(isset($_POST['value1'])) {
+						
+				$numeros = $_POST['value1'];
+						
+				$vetor= explode (" ", $numeros);
+				$pos=array();
+				$neg=array();
+					
+				foreach($vetor as $vet){
+					($vet <= 0) ? $neg[]=$vet : $pos[]=$vet;
+				} 
+					echo "<div class='results'>\n</div>" . "<div class='results'>Os vetores positivos são:</div>" . "<div class='results'>\n</div>" . "<div class='results'>\n</div>";
+						print_r($pos);
+							
+					echo "<div class='results'>\n</div>" . "<div class='results'>Os vetores negativos são:</div>" . "<div class='results'>\n</div>" . "<div class='results'>\n</div>";
+						print_r($neg);
+					
+					$total = array_sum($pos);
+					$count = count($neg);
+					
+					echo "<div class='results'>\n</div>" . "</div><div class='results'>Soma dos positivos: $total</div>";
+					echo "<div class='results'>\n</div>" . "</div><div class='results'>Quantidade de negativos: $count</div>";
 			}
-		 
 		?>
 		
 		</main>
 		
 		</body>
-		</html>
+	</html>
