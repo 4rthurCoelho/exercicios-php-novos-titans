f1f250fee9efe4440642b1ca6db9e99d65604e1d
4rthurCoelho <arthur.coelhojb@gmail.com>
2022-03-22 11:00:28 -0300

    ex14 feito com banco de dados

---
+++ ex14/Readme.md
@@ -0,0 +1,17 @@
+# More informations on How to run the codes of *ex14*
+
+#### Requirements
+- PHP
+- Docker
+- Docker Compose
+
+## How do you will run the project?
+- At the folder of the project, run the command `composer install` in your terminal
+- Run the command `php -S localhost:8081`
+
+## How to run the database?
+- Run the command: `docker-compose up -d`
+- Import the structure of the table with the file *resultados.sql* located in the project folder
+
+Tip: 
+- Look the `docker-compose.yml` to others settings.
---
+++ ex14/resultados.sql
@@ -0,0 +1,58 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ex14`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `Resultados`
+--
+
+CREATE TABLE `Resultados` (
+  `id` int NOT NULL,
+  `altura1` int NOT NULL,
+  `crescimento1` int NOT NULL,
+  `altura2` int NOT NULL,
+  `crescimento2` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `Resultados`
+--
+ALTER TABLE `Resultados`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `Resultados`
+--
+ALTER TABLE `Resultados`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
