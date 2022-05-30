4a9118057bd210b2cecb2c9d15720e44be41f3de
4rthurCoelho <arthur.coelhojb@gmail.com>
2022-05-30 16:22:18 -0300

    teste automatizado e adicionando botão do exercicio no indice

--- tests/acceptance/ExercicioNovosTitansCest.php
+++ tests/acceptance/ExercicioNovosTitansCest.php
@@ -357,4 +357,38 @@ class ExercicioNovosTitansCest
         25');
     }
 
+    public function ex19Works(AcceptanceTester $I)
+    {
+        $I->amOnPage('/ex19');
+        $I->see('Exercício 19');
+        //Teste
+        $I->fillField('value1', '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25');
+        $I->click('enviar');
+        $I->see('1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10
+        11
+        12
+        13
+        14
+        15
+        16
+        17
+        18
+        19
+        20
+        21
+        22
+        23
+        24
+        25');
+    }
+
 }
